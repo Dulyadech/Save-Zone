2872b1098b3937902ace15c19709bdc245ceab43
Dulyadech <dulyadech.p@kkumail.com>
2025-03-21 14:36:42 +0700

    latest today 21-3-2025

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from website import create_app
+""" Main to run server of website """
+from website import create_app, db
 
 app = create_app()
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run(debug=True)
